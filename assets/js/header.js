cade58421cf947011ab4e2e99686e305bd27fa29
Scott Taylor <se.t@email.com>
2026-08-04 19:46:48 -0500

    Removed Umami from GTM and placed in header js

--- assets/js/header.js
+++ assets/js/header.js
@@ -16,4 +16,22 @@
     contentLanguage.setAttribute("content", "en-US");
     head.appendChild(contentLanguage);
   }
+
+  /*
+   * Umami Analytics
+   */
+  if (
+    !head.querySelector(
+      'script[data-website-id="62eae9b2-24e3-41bf-8f07-021c1cc97cdd"]',
+    )
+  ) {
+    const umamiScript = document.createElement("script");
+    umamiScript.defer = true;
+    umamiScript.src = "https://cloud.umami.is/script.js";
+    umamiScript.setAttribute(
+      "data-website-id",
+      "62eae9b2-24e3-41bf-8f07-021c1cc97cdd",
+    );
+    head.appendChild(umamiScript);
+  }
 })();
