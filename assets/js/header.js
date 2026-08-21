0f473ea9bb51eccbd7acf464231d78de7059ff2f
Scott Taylor <se.t@email.com>
2026-08-21 05:06:44 -0500

    Fixing render-blocking requests for faster page load

--- assets/js/header.js
+++ assets/js/header.js
@@ -18,6 +18,52 @@
   }
 
   /*
+   * Critical CSS (moved out of main-min.css's @import rules
+   * so the browser can fetch them in parallel instead of serially)
+   */
+  if (
+    !head.querySelector(
+      'link[rel="preconnect"][href="https://fonts.googleapis.com"]',
+    )
+  ) {
+    const preconnectGoogleFonts = document.createElement("link");
+    preconnectGoogleFonts.rel = "preconnect";
+    preconnectGoogleFonts.href = "https://fonts.googleapis.com";
+    head.appendChild(preconnectGoogleFonts);
+  }
+
+  if (
+    !head.querySelector(
+      'link[rel="preconnect"][href="https://fonts.gstatic.com"]',
+    )
+  ) {
+    const preconnectGstatic = document.createElement("link");
+    preconnectGstatic.rel = "preconnect";
+    preconnectGstatic.href = "https://fonts.gstatic.com";
+    preconnectGstatic.crossOrigin = "anonymous";
+    head.appendChild(preconnectGstatic);
+  }
+
+  if (!head.querySelector('link[href="css/fontawesome-all.min.css"]')) {
+    const fontAwesome = document.createElement("link");
+    fontAwesome.rel = "stylesheet";
+    fontAwesome.href = "css/fontawesome-all.min.css";
+    head.appendChild(fontAwesome);
+  }
+
+  if (
+    !head.querySelector(
+      'link[href^="https://fonts.googleapis.com/css?family=Source+Sans+Pro"]',
+    )
+  ) {
+    const googleFonts = document.createElement("link");
+    googleFonts.rel = "stylesheet";
+    googleFonts.href =
+      "https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,300i,400,400i";
+    head.appendChild(googleFonts);
+  }
+
+  /*
    * Umami Analytics
    */
   if (
